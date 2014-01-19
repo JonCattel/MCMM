729f2cc40f03e612945806d291b27bfba833e741
Dian Solovey <soloveyd@wit.edu>
2014-01-19 00:01:19 -0500

    First Commit

---
+++ MyCaddieMasterMobile/gen/com/soloveyd/mycaddiemastermobile/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.soloveyd.mycaddiemastermobile;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ MyCaddieMasterMobile/src/com/soloveyd/mycaddiemastermobile/MenuActivity.java
@@ -0,0 +1,7 @@
+package com.soloveyd.mycaddiemastermobile;
+
+import android.app.Activity;
+
+public class MenuActivity extends Activity{
+
+}
